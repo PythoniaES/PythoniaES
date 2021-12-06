30ca6076a4c1865d4194829ca2162059392c55e4
PythonIA <90198356+PythoniaES@users.noreply.github.com>
2021-12-06 07:48:42 +0100

    Add files via upload

---
+++ CriptoBot/config.py
@@ -0,0 +1,19 @@
+
+
+BINANCE = {
+  "key": "",
+  "secret": ""
+}
+
+CRIPTO = 'ADA'
+REF = 'EUR'
+
+PERIOD = '1m'  #puede elegir 1m, 3m, 5m, 15m, 1h, 3h
+
+
+EMA_F = 5
+EMA_S = 15
+
+# capital que se quiere invertir
+CAPITAL = 15
+
---
+++ CriptoBot/main.py
@@ -0,0 +1,12 @@
+
+from src.Bot import CriptoBot_I
+from config import *
+
+api_key = BINANCE['key']
+api_secret = BINANCE['secret']
+
+
+
+bot = CriptoBot_I()
+bot.Set_Parameters(api_key=api_key, api_secret=api_secret, cripto=CRIPTO, ref=REF, period=PERIOD, capital=CAPITAL,EMA_S=EMA_S, EMA_F=EMA_F)
+bot.run()
